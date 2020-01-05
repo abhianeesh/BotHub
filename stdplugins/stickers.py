a8013bb765abf4d9c70d08890167286d149e010a
abhianeesh <59135313+abhianeesh@users.noreply.github.com>
2020-01-05 18:07:22 +0530

    Update stickers.py

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -44,9 +44,13 @@ async def _(event):
 
     me = borg.me
     userid = event.from_id
-    packname = f"Adarsh_T sticker pack"
-    packshortname = f"Adarsh_T{userid}"  # format: Adarsh_T_userid
+    packname = f"Adarsh T Sticker pack"
+    packshortname = f"Adarsh_T{userid}"  # format: Uni_Borg_userid
 
+    is_a_s = is_it_animated_sticker(reply_message)
+    file_ext_ns_ion = "Adarsh_Sticker.png"
+    file = await borg.download_file(reply_message.media)
+    uploaded_sticker = None
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "Adarsh_Sticker.png"
     file = await borg.download_file(reply_message.media)
@@ -54,11 +58,8 @@ async def _(event):
     if is_a_s:
         file_ext_ns_ion = "AnimatedSticker.tgs"
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
-        packname = f"{userid}'s @AnimatedStickersGroup"
-        if userid == 719877937:
-            packshortname = "SnapDragon_Animated"
-        else:
-            packshortname = f"Adarsh_T_{userid}_as" # format: Adarsh_T_userid
+        packname = f"Adarsh_AnimatedStickers"
+        packshortname = f"Adarsh_Animated"  # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
